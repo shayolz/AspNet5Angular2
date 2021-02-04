827586f3806b8dccca9a64569a70e00c29d1391e
Enrico Silvano <enrico.silvano@outlook.com>
2021-02-04 16:46:49 +0100

    Updated to angular 11 and clartiy 5

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -25,10 +25,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ]),
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'counter', component: CounterComponent },
+    { path: 'fetch-data', component: FetchDataComponent },
+], { relativeLinkResolution: 'legacy' }),
     ClarityModule,
     BrowserAnimationsModule
   ],
