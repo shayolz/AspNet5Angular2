da5c765ac7b426879e4e7752f6c07344301c3864
Enrico Silvano <enrico.silvano@outlook.com>
2021-02-05 09:34:05 +0100

    Moved project to subfolder

---
+++ Infrastructure/AppDbContextSection/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.AppDbContextSection
+{
+
+    public partial class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-69I5U7A\\SQLEXPRESS;Database=Test2;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
